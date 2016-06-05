2d294e6ee9f82621550ebe3ab2256f09e9d7518a
Kristof Van Cauwenbergh <kristof.vancauwenbergh@gmail.com>
2016-06-05 14:23:08 +0200

    Remove unused use statement

--- src/Form/Type/ContactType.php
+++ src/Form/Type/ContactType.php
@@ -18,7 +18,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
  * Class ContactType
